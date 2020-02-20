10d53e322efadee3e21c3748f0d9d6d714f0c7e6
Adema Beketayeva <oriharadem@gmail.com>
2020-02-20 17:48:58 +0600

    New routes

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,13 @@ Route::get('/faq/question/{id}', 'FaqController@get')->name('question.get');
 Route::post('/faq/question/create', 'FaqController@create')->name('question.create');
 Route::put('/faq/question/{id}/update', 'FaqController@update')->name('question.update');
 Route::delete('/faq/question/{id}/delete', 'FaqController@delete')->name('question.delete');
+Route::get('/faq/questions/with_tag', 'FaqController@allWithTags')->name('questions.with_tags');
+
+Route::get('/faq/tags', 'TagController@index')->name('tags.index');
+Route::get('/faq/tag/{id}', 'TagController@get')->name('tag.get');
+Route::post('/faq/tag/create', 'TagController@create')->name('tag.create');
+Route::put('/faq/tag/{id}/update', 'TagController@update')->name('tag.update');
+Route::delete('/faq/tag/{id}/delete', 'TagController@softDelete')->name('tag.softDelete');
+Route::get('/faq/tags/with_faqs', 'TagController@allWithFaqs')->name('tags.with_faqs');
+
+Route::get('/faq/categories/with_faq_tag', 'CategoryController@allWithQuestions')->name('categories.with_faq_tag');
